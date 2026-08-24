29eb49808955c51b5d64e1cdee2ccc62bebd1529
teenytinybit <juliana.toporkova@gmail.com>
2026-08-24 21:48:31 +0300

    remove endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import { handlerReadiness } from "../handlers/readiness.js";
-import { handlerValidateChirp } from "../handlers/validateChirp.js";
 import { handlerCreateUser } from "../handlers/users.js";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/healthz", handlerReadiness);
-apiRouter.post("/validate_chirp", handlerValidateChirp);
 apiRouter.post("/users", handlerCreateUser);
 export default apiRouter;
