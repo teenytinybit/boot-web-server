3e36e61c066a7a87ed571a7f70f456e2009f133c
teenytinybit <juliana.toporkova@gmail.com>
2026-08-24 21:48:00 +0300

    add new schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -10,4 +10,18 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 256 }).unique().notNull(),
 });
 
+export const chirps = pgTable("chirps", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  body: varchar("body", { length: 140 }).notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+});
+
 export type NewUser = typeof users.$inferInsert;
+export type NewChirp = typeof chirps.$inferInsert;
